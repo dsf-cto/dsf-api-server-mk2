58bb9e6a6569b381349d5f371b8ecb6a3aff7af9
AndreyAverin_DSF.Finance_CTO <122683424+dsf-cto@users.noreply.github.com>
2024-05-11 18:10:43 +0700

    Add files via upload

---
+++ api/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const serverless = require('serverless-http');
+
+const app = express();
+
+app.get('*', (req, res) => {
+  res.send('Hello World!');
+});
+
+module.exports = app;
+module.exports.handler = serverless(app);
