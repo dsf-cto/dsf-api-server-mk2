31070fd68b64fd9186713cdc2c809e92e0e1ce4f
AndreyAverin_DSF.Finance_CTO <122683424+dsf-cto@users.noreply.github.com>
2024-08-14 22:54:23 +0700

    Update index.js Mk6.7

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// DSF.Finance API Server Mk6
+// DSF.Finance API Server Mk6.7
 import { EventEmitter } from 'events';
 EventEmitter.defaultMaxListeners = 20;
 
@@ -166,9 +166,9 @@ testConnection();
 //         console.log(`Таблица успешно удалена`);
 
 // For RESTART DataBase contract_events: 
-// const dropTableQuery = `DROP TABLE IF EXISTS contract_events;`;
-//         await pool.query(dropTableQuery);
-//         console.log(`Таблица успешно удалена`);
+const dropTableQuery = `DROP TABLE IF EXISTS contract_events;`;
+        await pool.query(dropTableQuery);
+        console.log(`Таблица успешно удалена`);
 
 // For RESTART DataBase personal_yield_rate: 
 // const dropTableQuery = `DROP TABLE IF EXISTS personal_yield_rate;`;
@@ -2709,7 +2709,7 @@ const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 // Функция для обработки событий
 async function processEvent(event) {
 
-    await delay(900); // Задержка 900 миллисекунд
+    await delay(1200); // Задержка 1200 миллисекунд
 
     const block = await web3.eth.getBlock(event.blockNumber);
     const eventDate = new Date(Number(block.timestamp) * 1000);
