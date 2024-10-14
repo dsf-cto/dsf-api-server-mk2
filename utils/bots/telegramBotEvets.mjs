c84eec8badc3c86a9ab28d82be3f1f5539ca494f
AndreyAverin_DSF.Finance_CTO <122683424+dsf-cto@users.noreply.github.com>
2024-10-14 15:34:56 +0700

    Update telegramBotEvets.mjs

--- utils/bots/telegramBotEvets.mjs
+++ utils/bots/telegramBotEvets.mjs
@@ -1,26 +1,31 @@
-import { Telegraf } from 'telegraf';
+// import { Telegraf } from 'telegraf';
 
-const key = '7058666957:AAEaixTBtC50Xd0qDAUjeLyLryINvhjEc0c';
-const chatId = '-1002158927402';
-const bot = new Telegraf(key);
+// const key = '7058666957:AAEaixTBtC50Xd0qDAUjeLyLryINvhjEc0c';
+// const chatId = '-1002158927402';
+// const bot = new Telegraf(key);
 
-bot.start((ctx) => {
-    ctx.reply('Салам! Ща буду скидывать сюда инфу по транзакциям.');
-});
+// bot.start((ctx) => {
+//     ctx.reply('Салам! Ща буду скидывать сюда инфу по транзакциям.');
+// });
 
-bot.on('text', (ctx) => {
-    const text = ctx.message.text.toLowerCase();
-    if (text.includes('привет')) {
-        ctx.reply('Привет! Ты зачем сюда пишешь??');
-    } else {
-        ctx.reply('Я бот, не надо мне писать, я смотрю блокчейн, не отвлекай меня.');
-    }
-});
+// bot.on('text', (ctx) => {
+//     const text = ctx.message.text.toLowerCase();
+//     if (text.includes('привет')) {
+//         ctx.reply('Привет! Ты зачем сюда пишешь??');
+//     } else {
+//         ctx.reply('Я бот, не надо мне писать, я смотрю блокчейн, не отвлекай меня.');
+//     }
+// });
 
-bot.launch();
+// bot.launch();
+
+// export function sendMessageToChat(message) {
+//     bot.telegram.sendMessage(chatId, message)
+//         .then(() => console.log('Message sent:', message))
+//         .catch((error) => console.error('Ошибка при отправке сообщения:', error));
+// }
 
 export function sendMessageToChat(message) {
-    bot.telegram.sendMessage(chatId, message)
-        .then(() => console.log('Message sent:', message))
-        .catch((error) => console.error('Ошибка при отправке сообщения:', error));
+    console.log('bot.telegram');
 }
+
